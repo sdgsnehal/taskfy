f1ceb18ca77f81e4d3215b0a3dcb9d3a346f9844
sdgsnehal <sdgsnehal@gmail.com>
2024-05-25 14:23:03 +0530

    project setup

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Marketinglayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      {/* navbar */}
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default Marketinglayout;
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Marketingpage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">Marketing</div>
+  );
+};
+
+export default Marketingpage;
