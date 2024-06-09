63588bd709909f666e3d919ac7a9d441bac91dac
sdgsnehal <sdgsnehal@gmail.com>
2024-06-09 19:00:25 +0530

    refactor(middleware.ts): update isPublicRoute variable to include additional public routes for better security fix(middleware.ts): fix conditional check for isPublicRoute to correctly match the request URL and improve logic accuracy

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { url } from "inspector";
 import { NextRequest, NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/proctected"]);
-const isPublicRoute = ["/"];
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 export default clerkMiddleware((auth, req) => {
   const { redirectToSignIn } = auth();
-  if (isProtectedRoute(req)) auth().protect();
-  if (auth().userId && isPublicRoute) {
+
+  if (auth().userId && isPublicRoute(req)) {
     let path = "/select-org";
     if (auth().orgId) {
       path = `/organization/${auth().orgId}`;
@@ -15,7 +14,7 @@ export default clerkMiddleware((auth, req) => {
     const orgSelection = new URL(path, req.url);
     return NextResponse.redirect(orgSelection);
   }
-  if (!auth().userId && !isPublicRoute) {
+  if (!auth().userId && !isPublicRoute(req)) {
     return redirectToSignIn({ returnBackUrl: req.url });
   }
   if (
