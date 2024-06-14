8485599dcb567d6e2e64ffdb0666a1a1b2c04320
sdgsnehal <sdgsnehal@gmail.com>
2024-06-14 22:45:27 +0530

    refactor(Sidebar.tsx): refactor Sidebar component to include dynamic accordion functionality based on expanded state and user memberships data. Added loading skeleton and workspace creation button.

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -21,7 +21,55 @@ const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
   const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
     userMemberships: { infinite: true },
   });
-  return <div>Sidebar</div>;
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string): string[] => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">Workspaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <p key={organization.id}>{organization.id}</p>
+        ))}
+      </Accordion>
+    </>
+  );
 };
 
 export default Sidebar;
