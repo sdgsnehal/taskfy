dcad320aa2adaa4244d0f7bba12e40d554e6619a
sdgsnehal <sdgsnehal@gmail.com>
2024-06-20 23:27:22 +0530

    style(Sidebar.tsx): improve layout of loading skeleton components for better readability style(mobile-sidebar.tsx): add margin to the Button component for better spacing feat(nav-item.tsx): add Skeleton component to improve loading state for NavItem component feat(organization settings): create new SettingsPage component for organization settings page

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
   id: string;
@@ -95,5 +96,15 @@ export const NavItem = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  );
+};
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute"></Skeleton>
+      </div>
+      <Skeleton className="h-10 w-full"></Skeleton>
+    </div>
   );
 };
