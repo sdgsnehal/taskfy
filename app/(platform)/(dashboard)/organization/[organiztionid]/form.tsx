9878c02ad81c32ac3160c65f15659edb18db0578
sdgsnehal <sdgsnehal@gmail.com>
2024-07-14 18:56:05 +0530

    feat(create-board): add functionality to create a new board with a title feat(create-board): implement schema validation for the title field in CreateBoard feat(create-board): define InputType and ReturnType for createBoard action feat(form.tsx): integrate useAction hook to handle form submission for creating a board feat(use-action): create custom hook to handle asynchronous actions with options feat(create-safe-action): implement createSafeAction function for schema validation

--- app/(platform)/(dashboard)/organization/[organiztionid]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organiztionid]/form.tsx
@@ -1,20 +1,30 @@
 "use client";
-
-import { create } from "@/actions/createBoard";
+import { createBoard } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
 import { error } from "console";
 import { Divide } from "lucide-react";
 import { useFormState } from "react-dom";
 import FormInput from "./form-input";
 import FormButton from "./form-button";
+import { useAction } from "@/hooks/use-action";
 
 export const Form = () => {
-  const initialState = { message: null, errors: {} };
-  const [state, dispatch] = useFormState(create, initialState);
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    execute({ title });
+  };
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={state?.error} />
+        <FormInput errors={fieldErrors} />
         <FormButton />
       </div>
     </form>
