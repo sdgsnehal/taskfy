402938f8437b3d2dd2e7ca63c9911408f5e5bcb7
sdgsnehal <sdgsnehal@gmail.com>
2024-09-04 21:52:51 +0530

    popover

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,1 +1,52 @@
 "use client";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverClose,
+} from "@radix-ui/react-popover";
+import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/actions/create-board";
+import { FormInput } from "./form-input";
+import { FormSubmit } from "./form-submit";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+interface FormPopOverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+export const FormPopOver = ({
+  children,
+  side = "bottom",
+  sideOffset = 0,
+  align,
+}: FormPopOverProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Create Board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form>
+          <div className="space-y-4"></div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
