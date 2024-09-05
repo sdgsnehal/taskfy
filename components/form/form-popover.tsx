1108003586a8566eaa266eeff51697378cd60e65
sdgsnehal <sdgsnehal@gmail.com>
2024-09-05 21:28:45 +0530

    create board

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { toast } from "sonner";
 import {
   Popover,
   PopoverContent,
@@ -11,6 +12,7 @@ import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
+import { error } from "console";
 interface FormPopOverProps {
   children: React.ReactNode;
   side?: "left" | "right" | "top" | "bottom";
@@ -23,6 +25,20 @@ export const FormPopOver = ({
   sideOffset = 0,
   align,
 }: FormPopOverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data });
+      toast.success("Board created");
+    },
+    onError: (error) => {
+      console.log({ error });
+      toast.error(error);
+    },
+  });
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    execute({ title });
+  };
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
@@ -43,8 +59,16 @@ export const FormPopOver = ({
             <X className="h-4 w-4" />
           </Button>
         </PopoverClose>
-        <form>
-          <div className="space-y-4"></div>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput
+              id="title"
+              label="Board title"
+              type="text"
+              errors={fieldErrors}
+            ></FormInput>
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
         </form>
       </PopoverContent>
     </Popover>
