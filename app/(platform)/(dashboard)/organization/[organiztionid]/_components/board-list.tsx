e135ec1e949bb4d89dd32408693e36ae6eff03f0
sdgsnehal <sdgsnehal@gmail.com>
2024-09-10 21:22:12 +0530

    board details

--- app/(platform)/(dashboard)/organization/[organiztionid]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organiztionid]/_components/board-list.tsx
@@ -1,9 +1,11 @@
 import { FormPopOver } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
-import { HelpCircle, Link, User2 } from "lucide-react";
+import { HelpCircle, User2 } from "lucide-react";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const BoardList = async () => {
   const { orgId } = auth();
@@ -32,9 +34,8 @@ const BoardList = async () => {
             className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
             style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
           >
-            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition">
-              <p></p>
-            </div>
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">{board.title}</p>
           </Link>
         ))}
         <FormPopOver side="right" sideOffset={10}>
@@ -58,3 +59,17 @@ const BoardList = async () => {
 };
 
 export default BoardList;
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+    </div>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organiztionid]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organiztionid]/layout.tsx
@@ -1,5 +1,13 @@
 import { Divide } from "lucide-react";
 import { OrgControl } from "./_components/org-control";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs/server";
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
