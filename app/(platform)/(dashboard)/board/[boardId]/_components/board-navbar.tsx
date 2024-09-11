3422a970d12789e9c40c8f5071f39276e8a13267
sdgsnehal <sdgsnehal@gmail.com>
2024-09-11 20:47:22 +0530

    board navbar

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,3 +1,13 @@
-export const BoardNavbar = () => {
-  return <div>Board NavBar!</div>;
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { Board } from "@prisma/client";
+interface BoardNavbarProps {
+  data: Board;
+}
+export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
+  return (
+    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      Board NavBar!
+    </div>
+  );
 };
