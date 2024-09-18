c23189b88e9016a2b283738081c78ef26f9945a7
sdgsnehal <sdgsnehal@gmail.com>
2024-09-18 22:00:42 +0530

    list header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -11,7 +11,7 @@ export const ListItem = ({ data, index }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#d0cdd0] shadow-md pb-2">
-        <ListHeader />
+        <ListHeader data={data} />
       </div>
     </li>
   );
