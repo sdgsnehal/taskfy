f455dd50ce01e42773c483c9db67c4587cda0fb8
sdgsnehal <sdgsnehal@gmail.com>
2024-10-22 22:56:34 +0530

    card model

---
+++ components/modals/card-modal/header.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { FormInput } from "@/components/form/form-input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CardWithList } from "@/types";
+import { Layout } from "lucide-react";
+import { useState } from "react";
+
+interface HeaderProps {
+  data: CardWithList;
+}
+export const Header = ({ data }: HeaderProps) => {
+  const [title, setTitle] = useState(data?.title);
+  return (
+    <div className="flex items-start gap-x-3 mb-6 w-full">
+      <Layout className="h-5 w-5 mt-1 text-neutral-700" />
+      <div className="w-full">
+        <form>
+          <FormInput
+            id="title"
+            defaulValue={title}
+            className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+Header.Skeleton = function HeaderSkeleton() {
+  return (
+    <div className="flex items-start gap-x-3 mb-6">
+      <Skeleton className="h-6 w-6 mt-1 bg-neutral-200" />
+      <div></div>
+    </div>
+  );
+};
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { fetcher } from "@/lib/fetcher";
+import { CardWithList } from "@/types";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import { Header } from "./header";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
+  const onClose = useCardModal((state) => state.onClose);
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
   return (
-    <Dialog>
-      <DialogContent>Iam a model</DialogContent>
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <Header data={cardData} />
+      <DialogContent>{cardData?.title}</DialogContent>
     </Dialog>
   );
 };
