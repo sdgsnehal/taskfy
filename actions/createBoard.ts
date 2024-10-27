c44fdacbe77466696555c8432f4037f5ab48945c
sdgsnehal <sdgsnehal@gmail.com>
2024-10-27 19:07:09 +0530

    commented create Board

--- actions/createBoard.ts
+++ actions/createBoard.ts
@@ -1,43 +1,43 @@
-"use server";
-import { db } from "@/lib/db";
-import { error } from "console";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-import { z } from "zod";
-export type State = {
-  errors?: {
-    title?: string[];
-  };
-  message?: string | null;
-};
-const CreateBoard = z.object({
-  title: z.string().min(3, {
-    message: "Minimum length of 3 letters is required",
-  }),
-});
+// "use server";
+// import { db } from "@/lib/db";
+// import { error } from "console";
+// import { revalidatePath } from "next/cache";
+// import { redirect } from "next/navigation";
+// import { z } from "zod";
+// export type State = {
+//   errors?: {
+//     title?: string[];
+//   };
+//   message?: string | null;
+// };
+// const CreateBoard = z.object({
+//   title: z.string().min(3, {
+//     message: "Minimum length of 3 letters is required",
+//   }),
+// });
 
-export async function create(prevState: State, formData: FormData) {
-  const validatedFields = CreateBoard.safeParse({
-    title: formData.get("title"),
-  });
-  if (!validatedFields.success) {
-    return {
-      error: validatedFields.error.flatten().fieldErrors,
-      message: "Missing fields",
-    };
-  }
-  const { title } = validatedFields.data;
-  try {
-    await db.board.create({
-      data: {
-        title,
-      },
-    });
-  } catch (error) {
-    return {
-      message: "Database Error",
-    };
-  }
-  revalidatePath("/organization/org_2hH9qDaWRtxv7Yqmta03gy2mJVm");
-  redirect("/organization/org_2hH9qDaWRtxv7Yqmta03gy2mJVm");
-}
+// export async function create(prevState: State, formData: FormData) {
+//   const validatedFields = CreateBoard.safeParse({
+//     title: formData.get("title"),
+//   });
+//   if (!validatedFields.success) {
+//     return {
+//       error: validatedFields.error.flatten().fieldErrors,
+//       message: "Missing fields",
+//     };
+//   }
+//   const { title } = validatedFields.data;
+//   try {
+//     await db.board.create({
+//       data: {
+//         title,
+//       },
+//     });
+//   } catch (error) {
+//     return {
+//       message: "Database Error",
+//     };
+//   }
+//   revalidatePath("/organization/org_2hH9qDaWRtxv7Yqmta03gy2mJVm");
+//   redirect("/organization/org_2hH9qDaWRtxv7Yqmta03gy2mJVm");
+// }
